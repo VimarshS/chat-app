ae1bc4430a961a3d0863387dc91b6257173c6651
VimarshS <vimarshadarsh@gmail.com>
2026-02-22 22:27:54 +0530

    fix vercel for production

--- server/server.js
+++ server/server.js
@@ -37,17 +37,7 @@
      })
 
      // middlewares setup
-     // app.use(cors());
-     app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",
-      "http://localhost:5174",  // ✅ add this
-      "https://chat-app-rho-lovat-24.vercel.app"
-    ],
-    credentials: true
-  })
-);
+     app.use(cors());
      app.use(express.json({limit:'4mb'}));
 
      //Route setup
@@ -58,11 +48,10 @@
      //Connect to mongoDB
      await connectDB();
 
-//      if(process.env.NODE_ENV!=="production"){   
-//             const PORT = process.env.PORT || 5000;
-//      server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-// }
- const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+     if(process.env.NODE_ENV!=="production"){   
+            const PORT = process.env.PORT || 5000;
+     server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
+ 
 //Export server for Vercel deployment
 export default server;
